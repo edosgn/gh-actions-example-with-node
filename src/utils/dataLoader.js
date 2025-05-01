c0b468ed79a646b99fc2a9cbe2eca8ffab5e0fa8
Edgar Guerrero <eoguerre@nequi.com>
2025-05-01 15:40:12 -0500

    feat: api create

---
+++ src/utils/dataLoader.js
@@ -0,0 +1,62 @@
+const redisClient = require('../config/redisClient');
+const TTL = parseInt(process.env.CACHE_TTL) || 3600;
+
+const precargarVehiculos = async() => {
+    const vehiculos = [{
+            id: 1,
+            tipo: "carro",
+            marca: "Toyota",
+            modelo: "Corolla 2021",
+            cilindraje: 1800,
+            max_velocidad: 220,
+            color: "gris"
+        },
+        {
+            id: 2,
+            tipo: "moto",
+            marca: "Yamaha",
+            modelo: "MT-07",
+            cilindraje: 689,
+            max_velocidad: 200,
+            color: "negro"
+        },
+        {
+            id: 3,
+            tipo: "carro",
+            marca: "Chevrolet",
+            modelo: "Camaro 2021",
+            cilindraje: 1800,
+            max_velocidad: 220,
+            color: "rojo"
+        },
+        {
+            id: 4,
+            tipo: "carro",
+            marca: "Renault",
+            modelo: "Logan",
+            cilindraje: 1800,
+            max_velocidad: 220,
+            color: "gris"
+        },
+        {
+            id: 5,
+            tipo: "carro",
+            marca: "Toyota",
+            modelo: "Prado",
+            cilindraje: 1800,
+            max_velocidad: 220,
+            color: "gris"
+        },
+    ];
+
+    for (const vehiculo of vehiculos) {
+        const key = `vehiculo:${vehiculo.id}`;
+        const exists = await redisClient.exists(key);
+        if (!exists) {
+            await redisClient.setEx(key, TTL, JSON.stringify(vehiculo));
+            console.log(`✅ Precargado: ${key}`);
+        }
+    }
+};
+
+module.exports = precargarVehiculos;
